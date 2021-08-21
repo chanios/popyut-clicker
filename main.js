931e07fe3f7a09325008937bdd6712ab82ff4fc1
Chelos <chan022@outlook.com>
2021-08-21 22:00:52 +0700

    x

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { fork } = require("child_process")
 const { worker_count } = require("./config")
-let thread = worker_count//Yhread Here
+let thread = worker_count//Thread Here
 while (thread--) {
     fork("./worker")
 }
